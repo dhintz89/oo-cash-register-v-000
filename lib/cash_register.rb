cc076edcd3d4ef08db1ee824fb143055a8399c50
Dan Hintz <dhintz@live.com>
2018-09-03 17:25:04 +0000

    automatically backed up by learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,9 +1,10 @@
 class CashRegister
-  attr_accessor :total :employee_discount
+  attr_accessor :total :employee_discount :items :old_total
   
   def initialize(employee_discount)
     @total = 0
     @employee_discount = employee_discount
+    @items = []
   end
   
   def total
@@ -11,9 +12,21 @@ class CashRegister
   end
   
   def add_item(title, price, quantity=1)
-    @total += price
+    @old_total = self.total
+    self.total += price
+    @items << title
   end
   
   def apply_discount
-    @total * @employee_discount
+    self.total *= @employee_discount
+    return "Success, your new total is #{self.total}"
   end
+  
+  def items
+    @items
+  end
+  
+  def void_last_transaction
+    self.total - @old_total
+  end
+end
